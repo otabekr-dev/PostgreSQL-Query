2f526c498fa4b8f1db6ee86fcfd13c8f272f8f47
djumanov <djumanovdev@gmail.com>
2025-09-08 17:30:54 +0500

    aggregation funcitons

---
+++ aggregations.sql
@@ -0,0 +1,19 @@
+SELECT
+    SUM(price) AS total
+FROM sales;
+
+SELECT
+    COUNT(*) as jami_sales
+FROM sales;
+
+SELECT
+    AVG(quantity)
+FROM sales;
+
+SELECT
+    MAX(price)
+FROM sales;
+
+SELECT
+    MIN(price)
+FROM sales;

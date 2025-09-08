86fe4d58c77316ee5ed0fa049bedeb532c9d413e
djumanov <djumanovdev@gmail.com>
2025-09-08 17:14:37 +0500

    task01 filter

---
+++ task01.sql
@@ -0,0 +1,15 @@
+SELECT 
+    *
+FROM sales
+WHERE city = 'Tashkent';
+
+SELECT 
+    *
+FROM sales
+WHERE product = 'Phone';
+
+SELECT 
+    *
+FROM sales
+WHERE sale_date BETWEEN '2025-09-02' AND '2025-09-05';
+

36ed2d5837dde7f461695d5e91f4ecf445c76f3c
djumanov <djumanovdev@gmail.com>
2025-09-08 16:17:20 +0500

    insert data

---
+++ insert_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO 
+    sales (customer_name, city, product, quantity, price, sale_date) 
+VALUES
+    ('Ali', 'Tashkent', 'Laptop', 1, 800.00, '2025-09-01'),
+    ('Vali', 'Samarkand', 'Phone', 2, 500.00, '2025-09-02'),
+    ('Laylo', 'Bukhara', 'Tablet', 1, 300.00, '2025-09-03'),
+    ('Aziz', 'Tashkent', 'Phone', 3, 500.00, '2025-09-03'),
+    ('Dilnoza', 'Namangan', 'Laptop', 2, 800.00, '2025-09-04'),
+    ('Bobur', 'Samarkand', 'Laptop', 1, 800.00, '2025-09-04'),
+    ('Madina', 'Bukhara', 'Phone', 1, 500.00, '2025-09-05'),
+    ('Javlon', 'Tashkent', 'Tablet', 2, 300.00, '2025-09-05'),
+    ('Shahnoza', 'Andijan', 'Phone', 4, 500.00, '2025-09-06'),
+    ('Rustam', 'Samarkand', 'Laptop', 1, 800.00, '2025-09-06');
